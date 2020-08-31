196e3855ea8d3eff352e05ba12f0721c6453d243
zhangjun <zhangjun@163.com>
2020-08-31 21:18:00 +0800

    firsttt

---
+++ bin/streamserver.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+#  Copyright (c) 2019, MicroWing SAG All Rights Reserved.
+#----------------------------------------------------------------------------
+
+cygwin=false;
+darwin=false;
+os400=false;
+mingw=false;
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+MINGW*) mingw=true;;
+OS400*) os400=true;;
+Darwin*) darwin=true
+        if [ -z "$JAVA_VERSION" ] ; then
+             JAVA_VERSION="CurrentJDK"
+           else
+             echo "Using Java version: $JAVA_VERSION"
+           fi
+           if [ -z "$JAVA_HOME" ] ; then
+             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
+           fi
+           ;;
+esac
+
+# resolve links
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+
+# Only set CARBON_HOME if not already set
+[ -z "$CARBON_HOME" ] && CARBON_HOME=`cd "$PRGDIR/.." ; pwd`
+
+[ -z "$RUNTIME_HOME" ] && RUNTIME_HOME=`cd "$PRGDIR/../wso2/worker" ; pwd`
+
+###########################################################################
+NAME=start-worker
+
+
+WORKER_INIT_SCRIPT="$CARBON_HOME/wso2/worker/bin/carbon.sh"
+
+
+$WORKER_INIT_SCRIPT $*
+exit;
